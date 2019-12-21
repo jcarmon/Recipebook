9377b6cb12fb92dff46fa307d6a865c9672d70de
John <johnacarmon@gmail.com>
2019-12-21 15:59:31 +0100

    added validations and began view restructuring: recipes view

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ApplicationRecord
     belongs_to :recipe
+    validates :name, presence: true, length: { minimum:3} 
+    validates :amount, presence:true, numericality:{greater_than: 0}
+    validates :units, inclusion: { in: %w(g ml pcs),
+        message: "%{value} is not a valid unit" }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ApplicationRecord
     belongs_to :author
-    has_many :ingredients,  dependent: :destroy
+    has_many :ingredients, dependent: :destroy
+    validates :title, presence: true, length: { minimum:3} 
+    validates :difficulty, presence: true
+    validates :comment, presence: true
+    
 end
 
 
