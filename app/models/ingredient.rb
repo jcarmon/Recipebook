49139550bf18475aa66617b5bac0fe2c2a7dd37f
jcarmon <johnacarmon@gmail.com>
2020-01-06 13:41:49 +0100

    tweaking some more styles

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
     belongs_to :recipe
-    validates :name, presence: true, uniqueness:true, length: { minimum:3} 
+    validates :name, presence: true, uniqueness:true, length: { minimum:3}
     validates :amount, presence:true, numericality:{greater_than: 0}
     validates :units, inclusion: { in: %w(g ml pcs),
         message: "%{value} is not a valid unit" }
